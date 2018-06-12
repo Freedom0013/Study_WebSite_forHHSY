96a83a393f6371abb64cb866738cea497a0ea03e
Freedom0013 <846909723@qq.com>
2018-06-12 18:18:32 +0800

    添加jar包

--- Android_Project/app/src/main/java/com/studyforest/MainActivity.java
+++ Android_Project/app/src/main/java/com/studyforest/MainActivity.java
@@ -3,8 +3,13 @@ package com.studyforest;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * 主页Activity
+ * Title: MainActivity
+ * @date 2018/6/12 18:17
+ * @author Freedom0013
+ */
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
