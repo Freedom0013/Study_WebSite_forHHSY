a6de36d6b784b0925ce698e79973638f7d95914d
Freedom0013 <846909723@qq.com>
2018-06-12 23:12:36 +0800

    添加jar包2

--- Android_Project/app/src/main/java/com/studytree/view/MainActivity.java
+++ Android_Project/app/src/main/java/com/studytree/view/MainActivity.java
@@ -1,8 +1,16 @@
-package com.studytree;
+package com.studytree.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.studytree.R;
+
+/**
+ * 主页Activity
+ * Title: MainActivity
+ * @date 2018/6/12 19:16
+ * @author Freedom0013
+ */
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
