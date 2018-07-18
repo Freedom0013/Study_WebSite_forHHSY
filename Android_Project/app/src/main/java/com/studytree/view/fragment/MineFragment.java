d8ca1d77e3442705580d80dfce353c4421149b33
Freedom0013 <846909723@qq.com>
2018-07-18 21:12:52 +0800

    资讯、我的ToolBar、侧边栏隐藏登陆后布局

--- Android_Project/app/src/main/java/com/studytree/view/fragment/MineFragment.java
+++ Android_Project/app/src/main/java/com/studytree/view/fragment/MineFragment.java
@@ -5,7 +5,9 @@ import android.app.Activity;
 import android.view.View;
 
 import com.studytree.R;
+import com.studytree.view.MainActivity;
 import com.studytree.view.base.BaseFragment;
+import com.studytree.view.widget.StudyTreeTitleBar;
 
 /**
  * 我的页面Fragment
@@ -13,9 +15,11 @@ import com.studytree.view.base.BaseFragment;
  * @date 2018/7/10 17:16
  * @author Freedom0013
  */
-public class MineFragment extends BaseFragment {
+public class MineFragment extends BaseFragment implements StudyTreeTitleBar.TitleBarClickListener{
+    private static final String TAG = MineFragment.class.getSimpleName();
+    private StudyTreeTitleBar mine_tool;
     /** Activity对象 */
-    private Activity mActivity;
+    private MainActivity mActivity;
 
     /** 空参构造函数（必须） */
     public MineFragment(){}
@@ -26,12 +30,35 @@ public class MineFragment extends BaseFragment {
      */
     @SuppressLint("ValidFragment")
     public MineFragment(Activity activity) {
-        mActivity = activity;
+        mActivity = (MainActivity) activity;
     }
 
     @Override
     public View initView() {
         View mRootView = View.inflate(mActivity, R.layout.tab_mine, null);
+
+        //配置toolBar
+        mine_tool = mRootView.findViewById(R.id.mine_tool);
+        mine_tool.setTitleRightVisibility(false);
+        mine_tool.setTitle("个人中心");
+        mActivity.setSupportActionBar(mine_tool);
+        mine_tool.setOnTitleBarClickedListener(this);
+
         return mRootView;
+    }
+
+    @Override
+    public void onTitleLeftClicked() {
+        mActivity.main_residelayout.openPane();
+    }
+
+    @Override
+    public void onTitleRightClicked() {
+
+    }
+
+    @Override
+    public void onTitleClicked() {
+
     }
 }
--- Android_Project/app/src/main/java/com/studytree/view/fragment/NewsFragment.java
+++ Android_Project/app/src/main/java/com/studytree/view/fragment/NewsFragment.java
@@ -5,7 +5,9 @@ import android.app.Activity;
 import android.view.View;
 
 import com.studytree.R;
+import com.studytree.view.MainActivity;
 import com.studytree.view.base.BaseFragment;
+import com.studytree.view.widget.StudyTreeTitleBar;
 
 /**
  * 资讯页面Fragment
@@ -13,7 +15,11 @@ import com.studytree.view.base.BaseFragment;
  * @date 2018/7/10 17:16
  * @author Freedom0013
  */
-public class NewsFragment extends BaseFragment {
+public class NewsFragment extends BaseFragment implements StudyTreeTitleBar.TitleBarClickListener{
+    private static final String TAG = NewsFragment.class.getSimpleName();
+    private MainActivity mActivity;
+    private StudyTreeTitleBar news_tool;
+
     /** 空参构造函数（必须） */
     public NewsFragment(){}
 
@@ -23,12 +29,35 @@ public class NewsFragment extends BaseFragment {
      */
     @SuppressLint("ValidFragment")
     public NewsFragment(Activity activity) {
-        mActivity = activity;
+        mActivity = (MainActivity) activity;
     }
 
     @Override
     public View initView() {
         View mRootView = View.inflate(mActivity, R.layout.tab_news, null);
+
+        //配置toolBar
+        news_tool = mRootView.findViewById(R.id.news_tool);
+        news_tool.setTitleRightVisibility(false);
+        news_tool.setTitle("最新资讯");
+        mActivity.setSupportActionBar(news_tool);
+        news_tool.setOnTitleBarClickedListener(this);
+
         return mRootView;
+    }
+
+    @Override
+    public void onTitleLeftClicked() {
+        mActivity.main_residelayout.openPane();
+    }
+
+    @Override
+    public void onTitleRightClicked() {
+
+    }
+
+    @Override
+    public void onTitleClicked() {
+
     }
 }
